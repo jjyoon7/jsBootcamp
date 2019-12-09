f535ddffe2869c484f0c56267b1903a7c14043d8
jjyoon7 <jay.yoon7@gmail.com>
2019-12-09 19:05:03 +0100

    if you do not declare the variables without var, const, let, it will save it to the global object.

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ init();
 //2. some words are reserved -> reserved words.
 
 globalThis
+
+//if you do not declare with var, const, let, it will save the value to the global object.
+//ex this will cause a problem, because this will overwrite the alert function of the window.
+alert  = 'hello';
+alert('he');
