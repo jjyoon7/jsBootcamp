aaa5fc160cfc8ac3b846a8f6948a508099ae9783
jjyoon7 <jay.yoon7@gmail.com>
2019-12-10 19:03:14 +0100

    if you use 'var' with same naming (t.ex. code example 'price'), the 'var' within the function scope shadows the global scope varialbe. This is called variable shadowing. 'let' and 'const' is a block-scoped, and does not cause this. They only exist within the scope.

--- index.js
+++ index.js
@@ -54,3 +54,17 @@ age3 = 26;
 //and you cannot change or update it.
 const age4 = 35;
 
+var price = 20;
+var isSale = true;
+
+// variable shadowing
+// let & const - block-scoped
+if (isSale) {
+  // discount price of product
+  var price = 20 -2;
+  const price = 20 - 2; 
+  // do something 
+  console.log('sale price', price);
+}
+
+console.log('price', price);
