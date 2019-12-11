1b2d4f3d2823a82aa81d4891b696f29b8580d3df
jjyoon7 <jay.yoon7@gmail.com>
2019-12-11 16:59:43 +0100

    another flow control method is 'switch'. When there are lots of condition and also conditions including strings, it is good to use switch instead of if else state. 'switch' contains case and default to specify what needs to happen based on what case. To prevent the code from running after meeting the condition, you must add 'break'. Check the code example.

--- index.js
+++ index.js
@@ -89,3 +89,19 @@ if (prefersDarkMode) {
   console.log('light mode set!');
   document.body.style.background = 'ghostwhite';
 }
+  
+const colorMode = 'dark';
+
+switch (colorMode) {
+  case 'solaried':
+      console.log('solarized mode set!'); 
+      document.body.style.background = 'palegoldenrod';
+      break;
+  case 'dark':
+      console.log('dark mode set!');  
+      document.body.style.background = 'black';
+      break;
+  default:
+      console.log('light mode set!');
+      document.body.style.background = 'ghostwhite';
+}
