c9ea1076fc73c7329f0dfe2ac6e06edb582fdebd
jjyoon7 <jay.yoon7@gmail.com>
2019-12-16 20:51:33 +0100

    closure ables to keep the track ro update the value. Usually when the local variable within the scope disappear after the function has been executed, but with the closure, you can update that local value and keep track of that value overtime. Important thing to remember is that you need to call a function in different scope than where function was originally defined. t.ex. you are calling the 'addLike()' by calling 'doubleLike()'

--- index.js
+++ index.js
@@ -145,3 +145,19 @@ function display(user, text) {
 
 const message = display(user1, "hello");
 console.log(message);
+
+function handleLikePost(step) {
+  let likeCount = 0;
+  return function addLike() {
+    likeCount += step;    
+    return likeCount;
+  }
+//   addLike();
+  console.log('like count:', likeCount);
+}
+
+const doubleLike = handleLikePost(2);
+
+console.log(doubleLike());
+console.log(doubleLike());
+console.log(doubleLike());
