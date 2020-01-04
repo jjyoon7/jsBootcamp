143f5ef0f41825f0c9e8da8ed9b2cf30d6fad93c
jjyoon7 <jay.yoon7@gmail.com>
2020-01-04 17:24:01 +0100

    'object' does not preserve the order. It is handy when you know the exact key value of the property to modify it, but if you don't, you can't really do much. This is when the 'array' comes in handy.

--- index.js
+++ index.js
@@ -398,3 +398,20 @@ const userData = {
 }
 
 userData.getBio();
+
+//flexible collections with arrays
+const todos = {};
+
+const todo1 = {
+  text: 'Wash the dishes',
+  complete: false  
+};
+
+const todo2 = {
+  text: 'Do laundry',
+  complete: false  
+};
+
+todos[1] = todo1;
+todos[2] = todo2;
+console.log(todos);
