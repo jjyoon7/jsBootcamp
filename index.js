53956a256b76bb5c4adfb7bd61977dc2bb3bfd6f
jjyoon7 <jay.yoon7@gmail.com>
2020-01-05 20:11:11 +0100

    '.includes()' is an array methods where you can check if the primitive data you are passing as an arguemtn, exists in the array.

--- index.js
+++ index.js
@@ -417,3 +417,13 @@ todos.pop();
 // todos[1] = todo2;
 // todos[0] = todo1;
 console.log(todos);
+
+//check elements in arrays
+const temperatures = [
+  { degrees: 69, isRecordTemp: false }, 
+  { degrees: 82, isRecordTemp: true }, 
+  { degrees: 73, isRecordTemp: false }, 
+  { degrees: 64, isRecordTemp: false }
+];
+
+console.log(temperatures.includes(50));
