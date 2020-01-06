8d23b394243d2e5e85514c08cdbfb38398df6c64
jjyoon7 <jay.yoon7@gmail.com>
2020-01-06 19:03:34 +0100

    .filter() returns an array that has met the conditions. In other words, filter will check if the return value of the element is true for every element. If nothing meets the conditoin, it returns an empty array []

--- index.js
+++ index.js
@@ -470,3 +470,15 @@ temperatures.map(temperature =>
     console.log(`Temperature ${temperature.degrees} was a record high last week!`);  
   }
 })
+
+//get subjsets of arrays.
+const restaurants = [
+  { name: 'Cap City Diner', milesAway: 2.2 },
+  { name: 'Chop Shop', milesAway: 4.1 },
+  { name: 'Northstar Cafe', milesAway: 0.9 },
+  { name: 'City Tavern', milesAway: 0.5 },
+  { name: 'Shake Shack', milesAway: 5.3 }
+]
+
+const results = restaurants.filter(restaurant => restaurant.name.startsWith('C'));
+console.log(results);
