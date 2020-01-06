6b3fd74e89fba4cc7d649fc6fed4413a9d71ee40
jjyoon7 <jay.yoon7@gmail.com>
2020-01-06 19:37:08 +0100

    '.reduce()' method executes a reducer function(that you provide, aka callback function) on each element of the array, and returns a single output value. In order to do so, it requires a accumulator where which will be the returned result value at the end and the initial value that it starts from. t.ex menuItems.reduce((accumulator, menuItem) => { return accumulator += menuItem.price; }, 0);

--- index.js
+++ index.js
@@ -484,3 +484,18 @@ const result = restaurants.find(restaurant =>
   restaurant.name.lowerCase().includes('north') && restaurant.milesAway < 3
 );
 console.log(result);
+
+const menuItems = [
+  { item: "Blue Cheese Salad", price: 8 },
+  { item: "Spicy Chicken Rigatoni", price: 18 },
+  { item: "Ponzu Glazed Salmon", price: 23 },
+  { item: "Philly Cheese Steak", price: 13 },
+  { item: "Baked Italian Chicken Sub", price: 12 },
+  { item: "Pan Seared Ribeye", price: 31 }
+];
+
+const priceAdded = menuItems.reduce((accumulator, menuItem) => {
+  return accumulator += menuItem.price;  
+}, 0);
+
+console.log(priceAdded)
