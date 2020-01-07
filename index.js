63291fb83cf9c244790facd36b6da3529c757e7d
jjyoon7 <jay.yoon7@gmail.com>
2020-01-07 22:23:55 +0100

    t.ex check the code how user data has been transformed from object to array and with reduce() array method, where you pass a condition to filter a data you needed.

--- index.js
+++ index.js
@@ -597,4 +597,10 @@ const users = {
 };
 
 //instead of using chain methods, consider reduce
-Object.entries(users).reduce();
+const usersOver20 = Object.entries(users).reduce((acc, [id, user]) => {
+  if (user.age > 20) {
+    acc.push({ ...user, id });
+  }  
+  return acc;
+}, []);
+console.log(usersOver20);
