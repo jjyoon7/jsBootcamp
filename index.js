76670dae2d89915e6782c325f183136335af7faf
jjyoon7 <jay.yoon7@gmail.com>
2020-01-12 20:01:01 +0100

    binding 'this' in constructor could prevent the issues where this data is not being passed property, because if property using 'this' is nested as 'callback' By binding the property in constructor with '.bind(this)', it could prevent those situations.

--- index.js
+++ index.js
@@ -743,3 +743,32 @@ class SaleProduct extends Product {
 }
 
 const product1 = new SaleProduct("Coffee Maker", 99, true, 20);
+
+const isAuth = true;
+const user = {
+  favorites: []
+};
+
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+    this.favoriteProduct = this.favoriteProduct.bind(this);
+  }
+
+  handleFavoriteProduct = () => {
+    if (isAuth) {
+      setTimeout(this.favoriteProduct, 1000);
+    } else {
+      console.log("You must be signed in to favorite products!");
+    }
+  }
+
+  favoriteProduct = () => {
+    user.favorites.push(this.name);
+    console.log(`${this.name} favorited!`);
+  }
+}
+
+const product1 = new Product('Coaster', 89.99)
+product1.handleFavoriteProduct()
