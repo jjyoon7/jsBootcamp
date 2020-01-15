f6ca0b4e7305ef9893f9f41df3a42a53e2841018
jjyoon7 <jay.yoon7@gmail.com>
2020-01-15 19:58:37 +0100

    check the example code how the fetch API could be re-written in simpler way with async and await

--- index.js
+++ index.js
@@ -844,3 +844,13 @@ async function getBlogPost() {
 }
 
 getBlogPost()
+
+const response = fetch('https://jsonplaceholder.typicode.com/posts/1');
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+async function getPost() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
+  const data = await response.json();
+  console.log(data);
+}
