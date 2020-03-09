3135f551bae82b6d0185c664a9cdfa5a3168e482
jjyoon7 <jay.yoon7@gmail.com>
2020-03-09 19:34:03 +0100

    'state' is a data to be managed in app

--- index.js
+++ index.js
@@ -1,101 +1,29 @@
-// this: reference to an object
+// state - data to be managed in app
 
-// 4 Rules to 'this'. How is it called?
+class App {
+  constructor() {
+    this.render();
+    this.$userMessage = document.getElementById("user-message");
+    this.checkAuth();
+  }
 
-// 1) in the global context
-// 2) as a method on an object
-// 3) as a constructor function or class constructor
-// 4) as a DOM event handler
+  checkAuth() {
+    const user = true;
+    if (user) {
+      this.$userMessage.textContent = "Welcome back!";
+    } else {
+      this.$userMessage.textContent = "You must sign in!";
+      this.$userMessage.style.color = "red";
+    }
+  }
 
-function whatIsThis() {
-  console.log(this);
-}
-
-whatIsThis();
-
-const userInfo = {
-  title: "Programmer",
-  user: {
-     first: 'Reed',
-     last: 'Barger',
-     greetUser() {
-      console.log(`Hi, ${this.first} ${this.last} ${this.title}`);  
-     }   
-  }  
-}
-
-userInfo.user.greetUser()
-
-class User {
-  constructor(first, age) {
-    this.first = first;
-    this.age = age;  
-  }  
-  
-  getAge() {
-    console.log(`${this.first}'s age is ${this.age}`);  
+  render() {
+    document.getElementById("root").innerHTML = `
+      <div>
+        <span id="user-message"></span>
+      </div>
+    `;
   }
 }
 
-const user = new User('Bob', 24);
-user.getAge();
-
-function User(first, age) {
-  this.first = first;
-  this.age = age;
-}
-
-User.prototype.getAge = function() {
-  console.log(`${this.first}'s age is ${this.age}`);  
-}
-
-const user2 = new User('jane', 25);
-user2.getAge();
-
-const button = document.createElement('button');
-button.textContent = "Click";
-document.body.appendChild(button);
-
-button.addEventListener('click', function() {
-  console.log(this);
-})
-
-// call(), apply(), bind()
-
-const user = {
-  name: "Reed",
-  title: "Programmer"  
-}
-
-function printUser() {
-  console.log(`${this.name} is a ${this.title}`);
-}
-
-printUser.call(user);
-
-const user = {
-  name: "Reed",
-  title: "Programmer"  
-}
-
-function printBio(city, country) {
-  console.log(`${this.name} is a ${this.title} in ${city}, ${country}`);
-}
-
-printBio.call(user, 'London', 'England')
-printBio.apply(user, ['London', 'England'])
-
-//once you use .bind() to determine 'this', it is determined for good.
-printUser.bind(user);
-
-const user = {
-  first: 'Bob',
-  fn() {
-     console.log(this.first); 
-  },
-  arrowFn: () => {
-    console.log(this.first);  
-  }
-}
-
-user.arrowFn();
+new App();
