03e4889d930dc7e06cc46cb20df08996b918c1e1
jjyoon7 <jay.yoon7@gmail.com>
2020-03-09 18:55:14 +0100

    'this' is a reference to an object. There are 4 rules to 'this', based on how it is called

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-import getDate from "./utils/date.js"
+// this: reference to an object
 
-class App {
-  constructor() {
-    this.render()  
-  }  
-  
-  render() {
-    document.getElementById('root').innerHTML = `
-      <div>Date: ${getDate()}</div>
-    `
-  }
-}
+// 4 Rules to 'this'. How is it called?
 
-new App()
+// 1) in the global context
+// 2) as a method on an object
+// 3) as a constructor function or class constructor
+// 4) as a DOM event handler
