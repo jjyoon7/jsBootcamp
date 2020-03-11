5f05bebfd9b5cbb2239a5d3e8ad99723fa53e266
jjyoon7 <jay.yoon7@gmail.com>
2020-03-11 21:36:11 +0100

    pure function returns reliable values, thus ideal to managing state.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 //reducer - helps us manage app state
-const reducer = (state, action) => newState
+// const reducer = (state, action) => newState
+
+function counterReducer(state, action) {
+  return state + 1;  
+}
+
+console.log(counterReducer(0) === 1);
