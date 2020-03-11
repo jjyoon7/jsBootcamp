62d2f6d5db2b9f633d4d81ad4b7640521bb6a143
jjyoon7 <jay.yoon7@gmail.com>
2020-03-11 21:46:36 +0100

    'state' should be immutable

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
 function counterReducer(state, action) {
   switch (action.type) {
     case 'INCREMENT':
-      return state + 1;
+      return {...state, count: state.count + 1;}
     case 'DECREMENT':
-      return state - 1;
+      return {...state, count: state.count - 1;}
     default:
       return state  
   }
